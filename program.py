455dfcbdc232df9ae48f7bd09e6b87072547ac5b
kouhumi <email@example.com>
2019-08-12 12:11:50 +0800

    user interface

---
+++ program.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from library import *
+'''
+Program Your Robot. Complete The Task.
+Write Your Program Below
+'''
+a = motor('A')
+run(a, 10,100 )
+'''
+Write Your Program Above
+Your Program is Awesome :)
+'''
+while True:
+    pass
